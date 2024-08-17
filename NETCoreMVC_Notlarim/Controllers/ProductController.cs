bb9752a3337bc0dbed328f0557a5d24f84d0ac8d
furkananil <furkananil960@gmail.com>
2024-08-17 16:04:23 +0300

    MVC Proje Altyapisi ve Temel Konfigurasyonlar

---
+++ NETCoreMVC_Notlarim/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NETCoreMVC_Notlarim.Models;
+
+namespace NETCoreMVC_Notlarim.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetProducts()
+        {
+            Product product = new Product();
+
+            //ViewResult result = View(""); belirtilen view ismindeki view dosyasını render eder.
+            //return result;
+            return View();
+        }
+    }
+}
---
+++ NETCoreMVC_Notlarim/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace NETCoreMVC_Notlarim.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
