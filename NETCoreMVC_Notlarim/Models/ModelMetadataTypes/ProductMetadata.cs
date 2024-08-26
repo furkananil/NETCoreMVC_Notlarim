d2a451d5763df7b754c2b010b872d67a5bf036eb
furkananil <furkananil960@gmail.com>
2024-08-26 16:42:16 +0300

    ModelMetadataType ile Validation Sorumlulugunu Baska Bir Class'a Yükleme

---
+++ NETCoreMVC_Notlarim/Models/ModelMetadataTypes/ProductMetadata.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NETCoreMVC_Notlarim.Models.ModelMetadataTypes
+{
+    public class ProductMetadata
+    {
+        [Required(ErrorMessage = "LUTFEN PRODUCT NAME GIRINIZ")]//ATTIRBUTES
+        [StringLength(100, ErrorMessage = "PRODUCT NAME EN FAZLA 100 KARAKTER OLABILIR")]
+        public string ProductName { get; set; }
+        [EmailAddress(ErrorMessage = "LUTFEN EMAIL FORMATINDA VERI GIRINIZ")]
+        public string Email { get; set; }
+    }
+}
--- NETCoreMVC_Notlarim/Models/Product.cs
+++ NETCoreMVC_Notlarim/Models/Product.cs
@@ -1,14 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using NETCoreMVC_Notlarim.Models.ModelMetadataTypes;
+using System.ComponentModel.DataAnnotations;
 
 namespace NETCoreMVC_Notlarim.Models
 {
+    [ModelMetadataType(typeof(ProductMetadata))] //BU SINIFIN VALIDATIONLARI PRODUCTMETADATADAN ALINACAKTIR
     public class Product
-    {
-        [Required(ErrorMessage = "LUTFEN PRODUCT NAME GIRINIZ")]//ATTIRBUTES
-        [StringLength(100, ErrorMessage = "PRODUCT NAME EN FAZLA 100 KARAKTER OLABILIR")]
+    { //VIEWMODEL YA DA ENTITYDE VALIDATION TANIMLAMAK YERINE MODELMETADATATYPEDA KULLANMAK SOLID PRENSIPLERINE GORE DAHA DOGRUDUR
+        
         public string ProductName { get; set; }
         public int Quantity { get; set; }
-        [EmailAddress(ErrorMessage = "LUTFEN EMAIL FORMATINDA VERI GIRINIZ")]
         public string Email { get; set; }
     }
 }
