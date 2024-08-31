1479bec6aadbd2e1ac5f8a7c597b6dd9a4d6fa0d
furkananil <furkananil960@gmail.com>
2024-08-31 12:24:21 +0300

    PartialView ile Moduler Tasarim ve PartialView Veri Gonderme

--- NETCoreMVC_Notlarim/Controllers/HomeController.cs
+++ NETCoreMVC_Notlarim/Controllers/HomeController.cs
@@ -6,7 +6,22 @@ namespace NETCoreMVC_Notlarim.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            //var images = new List<string> { "img1.jpg", "img2.jpg", "img3.jpg" };
+
+            //return View(images);
+
+            ViewBag.Data = new List<string> { "img1.jpg", "img2.jpg", "img3.jpg" };
+
+            object o = new object();
+            return View(o);
+
+            //BU DATA LAYOUTA GELECEK ORADAN _SLAYTPARTIALA GELIp =>
+            //
+            //@model List<string>
+            // var m = Model;
+            
+            // LAYOUTTA
+            // <partial name="/Views/Home/Partials/_SlaytPartial.cshtml" model="@ViewBag.Data / >
         }
         public IActionResult Index2()
         {
