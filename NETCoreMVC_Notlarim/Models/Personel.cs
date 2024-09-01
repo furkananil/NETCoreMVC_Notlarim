17071d8cff65179ec2b645970f59eee7add79bdf
furkananil <furkananil960@gmail.com>
2024-09-01 19:20:53 +0300

    ViewComponent Yapilanmasi

---
+++ NETCoreMVC_Notlarim/Models/Personel.cs
@@ -0,0 +1,8 @@
+﻿namespace NETCoreMVC_Notlarim.Models
+{
+    public class Personel
+    {
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+    }
+}
---
+++ NETCoreMVC_Notlarim/ViewComponents/PersonellerViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NETCoreMVC_Notlarim.Models;
+
+namespace NETCoreMVC_Notlarim.ViewComponents
+{
+    public class PersonellerViewComponent : ViewComponent
+    {
+        //VIEWCOMPONENTIN IMZASI BU SEKILDE OLMALIDIR
+        public IViewComponentResult Invoke()
+        {
+            List<Personel> data = new() 
+            {
+                new Personel { Adi = "Furkan" , Soyadi = "5"},
+                new Personel { Adi = "Furkan" , Soyadi = "0"},
+                new Personel { Adi = "Furkan" , Soyadi = "1"},
+                new Personel { Adi = "Furkan" , Soyadi = "2"},
+                new Personel { Adi = "Furkan" , Soyadi = "3"},
+                new Personel { Adi = "Furkan" , Soyadi = "4"}
+            };
+
+            return View(data);
+        }
+    }
+}
